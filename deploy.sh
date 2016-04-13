71cb923aa7e02553476b8e0076eba1e2c1cffddc
Pere Albujer <p4r.esp@gmail.com>
2016-04-14 01:21:43 +0200

    [MOD] Modifications in deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,25 +4,25 @@ set -e # exit with nonzero exit code if anything fails
 gem install bundler
 bundle install
 
-# run commands to build the website in the out folder
+# Add source from master branch
+git clone https://github.com/P4R/p4r.github.io.git -b master dist
+
+# Run commands to build the website in the dist folder
 npm install -g gulp bower && npm install && bower install
 gulp
 
-# go to the out directory and create a *new* Git repo
+# Go to the dist directory.
 cd dist
-git init
 
-# inside this git repo we'll pretend to be a new user
+# Inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
 git config user.email "p4r.esp@gmail.com"
 
-# The first and only commit to this new Git repo contains all the
-# files present with the commit message "Deploy".
+# Add changes and commit.
 git add .
-git commit -m "Deploy "
+git commit -m "Deploy with Travis CI"
 
-# Force push from the current repo's master branch to the remote
-# repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
+# Push from the current repo's master branch to the remote
+# repo's master branch. We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@github.com/P4R/p4r.github.io.git" master:gh-pages > /dev/null 2>&1
+git push --quiet "https://${GH_TOKEN}@github.com/P4R/p4r.github.io.git" master > /dev/null 2>&1
